7c19e4120b1208238171752fd45a9936767f76b9
Alexjandro Mendoza <alexjandromendoza@macbook-pro.lan>
2022-01-25 17:53:11 -0500

    added feedbackStats to project

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,11 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
 
 
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
 
   
     
@@ -14,6 +15,10 @@ function FeedbackItem({item}) {
     <Card >
 
       <div className='num-display'>{item.rating}</div>
+      <button onClick = {() => handleDelete(item.id)}
+       className='close'>
+        <FaTimes color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
       
   </Card>
