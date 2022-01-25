62c2acb7700e4bb72af66b29cc6340adbdae76b3
Alexjandro Mendoza <alexjandromendoza@macbook-pro.lan>
2022-01-25 17:25:43 -0500

    card component working fine

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import React from 'react' 
-
-
-
-
+import {useState} from 'react'
 import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from './data/FeedbackData'
 
 
 function App() {
-    
-
+    const [feedback, setFeedback] = useState (FeedbackData)
 
     return (
         <>
             <Header />
             <div className='container'> 
-            <h1>My App</h1>
+            <FeedbackList feedback={feedback}/>
             </div>
-        </>
-
-    
+        </>   
     )
 }
 
 export default App
 
-
-
-//Anything major - git add * 
-// Git status 
-// Git commit -m "message here" 
-//Git push
-
-//Git log (commit history)
-//GIT CHECKOUT "BRANCH NAME" - Switches branches
-//Git pull to pull branches
-//Git merge PadInput/ or branch name
-
-//Source control in VS accept current changes for package-lock.json
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,22 @@
+const FeedbackData = [
+    {
+        id:1,
+        rating:9,
+        text: 'Best food i have ever had !',
+    },
+
+    {
+        id:2,
+        rating:3,
+        text: 'They sucked',
+    },
+
+    {
+        id:3,
+        rating:7,
+        text: 'They were okay',
+    },
+
+]
+
+export default FeedbackData
